c69308aa4d74541d31b897b75c2c80ae268e2018
WesleyFirmino <wesleydasilva_firmino@live.com>
2021-06-18 21:47:31 -0300

    Setting Class Adapter

--- app/src/main/java/com/devventurus/whatdoilearn/view/LearnedItemAdapter.kt
+++ app/src/main/java/com/devventurus/whatdoilearn/view/LearnedItemAdapter.kt
@@ -1,28 +1,43 @@
 package com.devventurus.whatdoilearn.view
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.devventurus.whatdoilearn.R
+import com.devventurus.whatdoilearn.data.LearnedItem
 
 class LearnedItemAdapter : RecyclerView.Adapter<LearnedItemAdapter.LearnedItemViewHolder>() {
 
-    inner class LearnedItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
+    var learnedItems = listOf<LearnedItem>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LearnedItemViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.learned_item, parent, false)
 
+        return LearnedItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: LearnedItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val learnedItem = learnedItems[position]
 
+        holder.bind(learnedItem)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-        
+        return learnedItems.size
     }
 
+    inner class LearnedItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val titleContainer : TextView = itemView.findViewById(R.id.learnedItemTitle)
+        private val descriptionContainer : TextView = itemView.findViewById(R.id.learnedItemDescription)
+
+        fun bind(learnedItem: LearnedItem) {
+            titleContainer.text = learnedItem.name
+            descriptionContainer.text = learnedItem.description
+
+        }
+
+    }
 }
